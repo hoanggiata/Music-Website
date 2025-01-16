3b9aa5802cc7233f217f6c46b3426cbce073d5d8
tulathanhhoang1281 <thanhhoang281202@gmail.com>
2025-01-16 19:09:32 +0700

    Add Album Page Mobile First

--- src/app/album/page.tsx
+++ src/app/album/page.tsx
@@ -1,8 +1,14 @@
+import LeftAlbum from "@/components/AlbumPage/LeftAlbum"
+import MusicAlbumCard from "@/components/AlbumPage/MusicAlbumCard"
+import NgheSiCard from "@/components/AlbumPage/NgheSiCard"
 import * as React from "react"
+
 export default function Album() {
     return (
-        <main>
-            <div></div>
+        <main className="min-h-screen bg-layout text-white relative flex flex-col overflow-hidden">
+            <LeftAlbum/>
+            <MusicAlbumCard/>
+            <NgheSiCard/>
         </main>
     )
 }
--- src/components/AlbumPage/NgheSiCard.tsx
+++ src/components/AlbumPage/NgheSiCard.tsx
@@ -1,6 +1,25 @@
-"use client"
-export default function NgheSiCard () {
-    return (
-        <div>NgheSiCard</div>
-    )
+"use client";
+import Image from "next/image";
+export default function NgheSiCard() {
+  return (
+    <div className="p-6 item-center">
+      <div className="flex justify-between items-center pb-2 ml-4">
+        <p className="text-base font-semibold">Nghệ Sĩ/OA</p>
+      </div>
+      <div className="grid grid-cols-2 gap-6">
+        <div className="flex flex-col items-center text-center mt-4">
+          <div className="w-32 h-32 rounded-full overflow-hidden">
+            <Image src="/image/artist.jpg" alt="Artist Cover" width={160}  height={160} className="object-cover"/>
+          </div>
+          <div className="rounded-lg px-5 py-4">
+            <h2 className="text-sm font-bold">Hồ Ngọc Hà</h2>
+            <p className="text-sm text-gray-500">423k người quan tâm</p>
+            <button className="px-6 py-2 mt-4 text-sm font-semibold text-white bg-blue-600 rounded-full">
+              Quan tâm
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
